ef8e2b8c30310c5fcd78f496e1b76947281cf19c
lemon710 <1020287311@qq.com>
2019-02-04 17:10:52 +0800

    删除分类模块

--- src/main/java/com/lym/tmall/service/CategoryService.java
+++ src/main/java/com/lym/tmall/service/CategoryService.java
@@ -31,4 +31,7 @@ public class CategoryService {
     public void add(Category bean) {
         categoryDAO.save(bean);
     }
+    public void delete(int id){
+        categoryDAO.delete(id);
+    }
 }
