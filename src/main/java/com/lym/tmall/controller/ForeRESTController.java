944dc6ffe533bfc781644256570843c7269c6df9
lemon710 <1020287311@qq.com>
2019-02-11 17:45:51 +0800

    前台用户注册模块

--- src/main/java/com/lym/tmall/controller/ForeRESTController.java
+++ src/main/java/com/lym/tmall/controller/ForeRESTController.java
@@ -1,11 +1,17 @@
 package com.lym.tmall.controller;
 
 import com.lym.tmall.pojo.Category;
+import com.lym.tmall.pojo.User;
 import com.lym.tmall.service.CategoryService;
 import com.lym.tmall.service.ProductService;
+import com.lym.tmall.service.UserService;
+import com.lym.tmall.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -15,6 +21,8 @@ public class ForeRESTController {
     CategoryService categoryService;
     @Autowired
     ProductService productService;
+    @Autowired
+    UserService userService;
 
     @GetMapping("/forehome")
     public Object home() {
@@ -25,4 +33,23 @@ public class ForeRESTController {
         return cs;
     }
 
+    @PostMapping("/foreregister")
+    public Object register(@RequestBody User user) {
+        String name =  user.getName();
+        String password = user.getPassword();
+        name = HtmlUtils.htmlEscape(name);
+        user.setName(name);
+        boolean exist = userService.isExist(name);
+
+        if(exist){
+            String message ="用户名已经被使用,不能使用";
+            return Result.fail(message);
+        }
+
+        user.setPassword(password);
+
+        userService.add(user);
+
+        return Result.success();
+    }
 }
--- src/main/java/com/lym/tmall/dao/UserDAO.java
+++ src/main/java/com/lym/tmall/dao/UserDAO.java
@@ -4,5 +4,5 @@ import com.lym.tmall.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDAO extends JpaRepository<User,Integer> {
-
+    User findByName(String name);
 }
