cd24a9d40be6e0957416e365de0c6184107cb491
lemon710 <1020287311@qq.com>
2019-02-13 04:11:53 +0800

    购物车详情页

--- src/main/java/com/lym/tmall/controller/ForeRESTController.java
+++ src/main/java/com/lym/tmall/controller/ForeRESTController.java
@@ -205,4 +205,12 @@ public class ForeRESTController {
         buyoneAndAddCart(pid,num,session);
         return Result.success();
     }
+
+    @GetMapping("forecart")
+    public Object cart(HttpSession session) {
+        User user =(User)  session.getAttribute("user");
+        List<OrderItem> ois = orderItemService.listByUser(user);
+        productImageService.setFirstProdutImagesOnOrderItems(ois);
+        return ois;
+    }
 }
