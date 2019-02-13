1475cac7d73b1c11684e93c9abb70f3c9dc7ce4f
lemon710 <1020287311@qq.com>
2019-02-13 21:12:14 +0800

    其他拦截器

--- src/main/java/com/lym/tmall/config/WebMvcConfigurer.java
+++ src/main/java/com/lym/tmall/config/WebMvcConfigurer.java
@@ -1,6 +1,7 @@
 package com.lym.tmall.config;
 
 import com.lym.tmall.interceptor.LoginInterceptor;
+import com.lym.tmall.interceptor.OtherInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -13,10 +14,16 @@ class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     public LoginInterceptor getLoginIntercepter() {
         return new LoginInterceptor();
     }
+    @Bean
+    public OtherInterceptor getOtherInterceptor() {
+        return new OtherInterceptor();
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(getLoginIntercepter())
+                .addPathPatterns("/**");
+        registry.addInterceptor(getOtherInterceptor())
                 .addPathPatterns("/**");
     }
 }
